8e563f0c2723a1778d7040f98b029ae9e474eba3
jayant <jayantparmar420@gmail.com>
2022-03-11 01:24:34 +0530

    Added welcome message in Line comparison program

---
+++ src/com/bridgelaqbz/LINE_COMPARISON.java
@@ -0,0 +1,12 @@
+package com.bridgelaqbz;
+
+public class LINE_COMPARISON {
+    public static void main(String[] args) {
+        LINE_COMPARISON object = new LINE_COMPARISON();
+        System.out.println("welcome to the Line comparison program");
+    }
+}
+
+
+
+
---
+++ src/com/bridgelaqbz/Main.java
@@ -0,0 +1,8 @@
+package com.bridgelaqbz;
+
+public class Main {
+
+    public static void main(String[] args) {
+
+    }
+}
