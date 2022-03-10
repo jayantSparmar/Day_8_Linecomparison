cdb773bf2187f3e7b7f5ba232ebdf5224a71ce9c
jayant <jayantparmar420@gmail.com>
2022-03-11 01:28:57 +0530

    Added Length of the line

--- src/com/bridgelaqbz/LINE_COMPARISON.java
+++ src/com/bridgelaqbz/LINE_COMPARISON.java
@@ -3,7 +3,15 @@ package com.bridgelaqbz;
 public class LINE_COMPARISON {
     public static void main(String[] args) {
         LINE_COMPARISON object = new LINE_COMPARISON();
+        object.UC_1();
         System.out.println("welcome to the Line comparison program");
+    }
+
+    public void UC_1() {
+        double x1 = 2, x2 = 5, y1 = 4, y2 = 6;
+        double length;
+        length = Math.sqrt((x2 - x1) * 2 + (y2 - y1) * 2);
+        System.out.println("length of the line : " + length);
     }
 }
 
